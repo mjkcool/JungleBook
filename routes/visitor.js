d23e0de968d0ab998d95e1cc0ed535ef56ec6c0b
minjeong <cpflqhdlrmsu@gmail.com>
2021-03-14 17:09:35 +0900

    deal routes

--- routes/visitor.js
+++ routes/visitor.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
+
 const VisitorController = require('../controllers/VisitorController')
 
-router.get('/', VisitorController.index)
+router.get('/', VisitorController.index) //get home
 router.post('/show', VisitorController.show)
 router.post('/store', VisitorController.store)
 
